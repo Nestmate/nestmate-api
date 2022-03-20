31cd0607d11bf4f09b95b9420f271301c8cd5d28
Rachouan Rejeb <info@rachouan.com>
2022-03-20 12:40:49 +0000

    Added JWT Sign In

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,9 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    firstName: String,
+    lastName: String,
+    email: {
       type: String,
       // unique: true -> Ideally, should be unique, but its up to you
     },
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const auth = require("../middleware/auth");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.get("/welcome",auth, (req, res, next) => {
+  res.json("Welcome 👋");
 });
 
 router.use("/auth", authRoutes);
